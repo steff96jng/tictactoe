3b2112d6a4fe577cb51be0a92bf826632b4a9b2d
Marcel Teiting <marcel.teiting@t-online.de>
2019-06-26 21:27:47 +0200

    Implementing one_hot

--- src/tttagent.py
+++ src/tttagent.py
@@ -29,12 +29,12 @@ class TTTAgent():
         https://www.youtube.com/watch?v=Qa2RxBXH4sU&t
     """
 
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size : int, action_size : int):
         """
         state size is going to contain the current board & color the agent has, so it's ~10
         """
         self.__gamma = 0.95   # discount rate
-        self.__epsilon = 0.7  # exploration rate
+        self.__epsilon = 0.7  # exploration rate, usually it's lower - e.g. .1-.3
         self.__action_size = action_size
         self.__state_size = state_size
 
@@ -45,7 +45,8 @@ class TTTAgent():
         # create a deep q model
         model = Sequential()
         model.add(Dense(units=24, activation='relu', input_dim=state_size))
-        model.add(Dense(units=24, activation='relu'))
+        model.add(Dense(units=72, activation='relu'))
+        model.add(Dense(units=72, activation='relu'))
         model.add(Dense(units=action_size, activation='linear'))
         model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
         # adam => https://en.wikipedia.org/wiki/Stochastic_gradient_descent
@@ -58,11 +59,29 @@ class TTTAgent():
         e.g. trying to place on an occupied field
         ==> punish the agent!
         """
-        if np.random.rand() <= self.__epsilon:          # make our own move
+        if np.random.rand() <= self.__epsilon:          # make our own move?
             return random.randrange(self.__state_size)  # make rnd move
 
-        act_values = self.__model.predict(state)        # return the move the ai makes
+        act_values = self.__model.predict(state)        # return the move the would ai make
         return np.argmax(act_values)
+
+    def __one_hot(self, state):
+        one_hot_state = []
+        for field in state:
+            if field == 0: # append state for empty field
+                one_hot_state.append(1)
+                one_hot_state.append(0)
+                one_hot_state.append(0)
+            elif field == 1: # append state for field player 1 (x)
+                one_hot_state.append(0)
+                one_hot_state.append(1)
+                one_hot_state.append(0)
+            else: # append state for field player 2 (x)
+                one_hot_state.append(0)
+                one_hot_state.append(0)
+                one_hot_state.append(1)
+
+        return one_hot_state
 
     def remember_state(self, state):
         pass
