8f6c7f8a31dea68f8a60571d1d0b66e837f63fc4
mteiting <marcel.teiting@t-online.de>
2019-07-13 14:07:10 +0200

    stuff

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-
 import random
 import argparse
 import sys
@@ -6,74 +5,99 @@ import tictactoe
 from player import HumanPlayer, RandomPlayer, AgentPlayer
 
 if __name__ == "__main__":
-  debug = False
-  ttt = tictactoe.TicTacToe()
-  p1 = RandomPlayer() # change this player to the player you like
-  p2 = AgentPlayer()
+    debug = False
+    ttt = tictactoe.TicTacToe()
+    p1 = AgentPlayer()  # change this player to the player you like
+    p2 = AgentPlayer()
 
-  p1.set_symbol(ttt.x)
-  p2.set_symbol(ttt.o)
+    p1.set_symbol(ttt.x)
+    p2.set_symbol(ttt.o)
 
-  active_player = p1
+    active_player = p1
 
-  stats = {'x': 0, 'o': 0, 'draw': 0}
-  stat_history = []
-  print("Start Training")
-  x = 1000
-  y = 10
+    stat_history = []
+    print("Start Training")
+    x = 1000
+    y = 1000
 
-  while True:
-    for i in range(x):
-      while not ttt.game_over():
-        if debug:
-          ttt.draw_board()
+    while True:
+        stats = {
+            "x": 0,
+            "o": 0,
+            "draw": 0,
+            "x_illegal": 0,
+            "y_illegal": 0,
+            "tried_moves": 0,
+        }
+        for i in range(x):
+            while not ttt.game_over():
+                if debug:
+                    ttt.draw_board()
 
-        active_player.make_move(ttt)
+                active_player.make_move(ttt)
 
-        if active_player == p1:
-          active_player = p2
-        else:
-          active_player = p1
+                if active_player == p1:
+                    active_player = p2
+                else:
+                    active_player = p1
 
-      p1.update_history(ttt)
-      p2.update_history(ttt)
+            p1.update_history(ttt)
+            p2.update_history(ttt)
 
-      if debug:
-        ttt.draw_board()
+            if debug:
+                ttt.draw_board()
 
-      if (i % 200) == 0 or i == (x - 1):
-        print("Round", i, "finished")
+            if (i % 200) == 0 or i == (x - 1):
+                print("Round", i, "finished")
 
-      if ttt.winner == None:
-        stats['draw'] = stats['draw'] + 1
-      elif ttt.winner == ttt.x:
-        stats['x'] = stats['x'] + 1
-      elif ttt.winner == ttt.o:
-        stats['o'] = stats['o'] + 1
+            if ttt.winner == None:
+                stats["draw"] = stats["draw"] + 1
+            elif ttt.winner == ttt.x:
+                stats["x"] = stats["x"] + 1
+            elif ttt.winner == ttt.o:
+                stats["o"] = stats["o"] + 1
 
-      ttt.reset()
-      active_player = p1
+            stats["tried_moves"] += ttt.tried_moves
+            ttt.reset()
+            active_player = p1
 
-    # p1.train(ttt)
-    # p1.reset_history()
+        stats["x_illegal"] = p1.illegal_moves
+        stats["y_illegal"] = p2.illegal_moves
 
-    if y > 0:
-      p2.train(ttt)
-      y -= 1
+        p1.train(ttt)
+        p1.reset_history()
 
-    p2.reset_history()
+        p2.train(ttt)
+        p2.reset_history()
 
-    stat_history.append(stats)
-    stats = {'x': 0, 'o': 0, 'draw': 0}
-    overall_stats = { 'x': 0, 'o': 0, 'draw': 0}
+        stat_history.append(stats.copy())
+        overall_stats = {
+            "x": 0,
+            "o": 0,
+            "draw": 0,
+            "x_illegal": 0,
+            "y_illegal": 0,
+            "tried_moves": 0,
+        }
 
-    for stat in stat_history:
-      overall_stats['x'] += stat['x']
-      overall_stats['o'] += stat['o']
-      overall_stats['draw'] += stat['draw']
-      # print(stat)
+        for stat in stat_history:
+            overall_stats["x"] += stat["x"]
+            overall_stats["o"] += stat["o"]
+            overall_stats["draw"] += stat["draw"]
+            overall_stats["x_illegal"] += stat["x_illegal"]
+            overall_stats["y_illegal"] += stat["y_illegal"]
+            overall_stats["tried_moves"] += stat["tried_moves"]
 
-    overall_stats['x'] = overall_stats['x'] / (len(stat_history) * x)
-    overall_stats['o'] = overall_stats['o'] / (len(stat_history) * x)
-    overall_stats['draw'] = overall_stats['draw'] / (len(stat_history) * x)
-    print(overall_stats)
+        overall_stats["x"] = overall_stats["x"] / (len(stat_history) * x)
+        overall_stats["o"] = overall_stats["o"] / (len(stat_history) * x)
+        overall_stats["draw"] = overall_stats["draw"] / (len(stat_history) * x)
+
+        stats["x"] = stats["x"] / x
+        stats["o"] = stats["o"] / x
+        stats["draw"] = stats["draw"] / x
+        stats["x_illegal_rel"] = stats["x_illegal"] / stats["tried_moves"]
+        stats["y_illegal_rel"] = stats["y_illegal"] / stats["tried_moves"]
+
+        print("overall", overall_stats)
+        print("current", stats)
+
--- src/player.py
+++ src/player.py
@@ -11,159 +11,163 @@ from tictactoe import TicTacToe, LENGTH
 
 
 class BasePlayer:
-  def set_symbol(self, symbol: int):
-    self.symbol = symbol
+    def set_symbol(self, symbol: int):
+        self.symbol = symbol
 
-  def make_move(self, ttt: TicTacToe):
-    pass
+    def make_move(self, ttt: TicTacToe):
+        pass
 
-  def update(self, ttt: TicTacToe):
-    pass
+    def update(self, ttt: TicTacToe):
+        pass
 
-  def update_history(self, ttt: TicTacToe):
-    pass
+    def update_history(self, ttt: TicTacToe):
+        pass
 
 
 class HumanPlayer(BasePlayer):
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def make_move(self, ttt: TicTacToe):
-    while True:
-      coord = input('Enter coordinates x,y: ')
-      x,y = coord.split(',')
-      x = int(x)
-      y = int(y)
-      if ttt.make_move(x, y, self.symbol):
-        return
+    def make_move(self, ttt: TicTacToe):
+        while True:
+            coord = input("Enter coordinates x,y: ")
+            x, y = coord.split(",")
+            x = int(x)
+            y = int(y)
+            if ttt.make_move(x, y, self.symbol):
+                return
 
-  def update(self, ttt: TicTacToe):
-    pass
+    def update(self, ttt: TicTacToe):
+        pass
 
-  def update_history(self, ttt: TicTacToe):
-    pass
+    def update_history(self, ttt: TicTacToe):
+        pass
 
 
 class RandomPlayer(BasePlayer):
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def make_move(self, ttt: TicTacToe):
-    while True:
-      coord = np.random.randint(3, size=2)
-      x = coord[0]
-      y = coord[1]
+    def make_move(self, ttt: TicTacToe):
+        while True:
+            coord = np.random.randint(3, size=2)
+            x = coord[0]
+            y = coord[1]
 
-      if ttt.make_move(x,y, self.symbol):
-        return
+            if ttt.make_move(x, y, self.symbol):
+                return
 
-  def update(self, ttt: TicTacToe):
-    pass
+    def update(self, ttt: TicTacToe):
+        pass
 
-  def update_history(self, ttt: TicTacToe):
-    pass
+    def update_history(self, ttt: TicTacToe):
+        pass
 
 
 class AgentPlayer(BasePlayer):
-  def __init__(self, epsilon = 0.7, alpha = 0.5, debug = False):
-    """
-    parameters:
+    def __init__(self, epsilon=0.7, alpha=0.5, debug=False):
+        """
+        parameters:
 
-    epsilon: greediness, probability of taking a random action instead of a greedy action
+        epsilon: greediness, probability of taking a random action instead of a greedy action
 
-    alpha: learning rate
-    """
-    self.debug = debug
-    self.history = []
-    self.epsilon = epsilon
-    self.alpha = alpha
-    self.model = self.__build_model()
+        alpha: learning rate
+        """
+        self.debug = debug
+        self.history = []
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.model = self.__build_model()
+        self.illegal_moves = 0
 
-  def update_history(self, ttt: TicTacToe):
-    result = 0
-    if ttt.game_over() and ttt.is_draw():
-      result = 0
-    elif ttt.game_over() and ttt.winner == self.symbol:
-      result = 1
-    else:
-      result = -1
+    def update_history(self, ttt: TicTacToe):
+        result = 0
+        if ttt.game_over() and ttt.is_draw():
+            result = 0
+        elif ttt.game_over() and ttt.winner == self.symbol:
+            result = 1
+        else:
+            result = -1
 
-    self.history.append((ttt.get_state(), result))
+        self.history.append((ttt.get_state(), result))
 
-  def make_move(self, ttt: TicTacToe):
-    def rnd_move():
-      while True:
-        coord = np.random.randint(3, size=2)
-        x = coord[0]
-        y = coord[1]
+    def make_move(self, ttt: TicTacToe):
+        def rnd_move():
+            while True:
+                coord = np.random.randint(3, size=2)
+                x = coord[0]
+                y = coord[1]
 
-        if ttt.make_move(x,y, self.symbol) or ttt.game_over():
-          return
+                if ttt.make_move(x, y, self.symbol) or ttt.game_over():
+                    return
 
-    if np.random.rand() < self.epsilon:
-      rnd_move()
+        if np.random.rand() > self.epsilon:
+            rnd_move()
 
-    state = ttt.get_state()
-    one_hot = self.one_hot_encoded(state, ttt)
+        state = ttt.get_state()
+        one_hot = self.one_hot_encoded(state, ttt)
 
-    values = self.model.predict(np.asarray([one_hot]))
-    max = np.argmax(values)
-    x = max % 3
-    y = max // 3
-    if  ttt.make_move(x, y, self.symbol):
-      return
-    else:
-      rnd_move()
+        values = self.model.predict(np.asarray([one_hot]))
+        max = np.argmax(values)
+        x = max % 3
+        y = max // 3
+        if ttt.make_move(x, y, self.symbol):
+            return
+        else:
+            self.illegal_moves += 1
+            rnd_move()
 
+    def train(self, ttt: TicTacToe):
+        states = []
+        q_values = []
 
-  def train(self, ttt: TicTacToe):
-    states = []
-    q_values = []
+        for game_state in self.history:
+            state, result = game_state
+            states.append(self.one_hot_encoded(state, ttt))
+            q_values.append(result)
 
-    for game_state in self.history:
-      state, result = game_state
-      states.append(self.one_hot_encoded(state, ttt))
-      q_values.append(result)
+        self.model.fit(
+            np.asarray(states), np.asarray(q_values), epochs=10, batch_size=len(states)
+        )
 
-    self.model.fit(np.asarray(states), np.asarray(q_values), epochs=10, batch_size=len(states))
-
-
-  def one_hot_encoded(self, state: [int], ttt: TicTacToe):
-    """
+    def one_hot_encoded(self, state: [int], ttt: TicTacToe):
+        """
     one_hot_encoded returns the game state as one hot encoded
     """
-    one_hot_state = []
+        one_hot_state = []
 
-    for field in state:
-      if field == ttt.x:
-        one_hot_state.append(1)
-        one_hot_state.append(0)
-      elif field == ttt.o:
-        one_hot_state.append(0)
-        one_hot_state.append(1)
-      elif field == ttt.empty:
-        one_hot_state.append(0)
-        one_hot_state.append(0)
-      else:
-        raise Exception('How?')
+        for field in state:
+            if field == ttt.x:
+                one_hot_state.append(1)
+                one_hot_state.append(0)
+            elif field == ttt.o:
+                one_hot_state.append(0)
+                one_hot_state.append(1)
+            elif field == ttt.empty:
+                one_hot_state.append(0)
+                one_hot_state.append(0)
+            else:
+                raise Exception("How?")
 
-    if self.symbol == ttt.x:
-      one_hot_state.append(1)
-    elif self.symbol == ttt.o:
-      one_hot_state.append(0)
-    else:
-      raise Exception('How?')
+        if self.symbol == ttt.x:
+            one_hot_state.append(1)
+        elif self.symbol == ttt.o:
+            one_hot_state.append(0)
+        else:
+            raise Exception("How?")
 
-    return one_hot_state
+        return one_hot_state
 
-  def reset_history(self):
-    self.history = []
+    def reset_history(self):
+        self.history = []
+        self.illegal_moves = 0
 
-  def __build_model(self):
-    model = Sequential()
-    model.add(Dense(units=128, activation='relu', input_dim=19)) # input layer
-    model.add(Dense(units=64, activation='relu')) #
-    model.add(Dense(units=64, activation='relu')) #
-    model.add(Dense(units=32, activation='relu')) # output layer
-    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-    return model
+    def __build_model(self):
+        model = Sequential()
+        model.add(Dense(units=128, activation="relu", input_dim=19))  # input layer
+        model.add(Dense(units=64, activation="relu"))  #
+        model.add(Dense(units=64, activation="relu"))  #
+        model.add(Dense(units=9, activation="relu"))  # output layer
+        model.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
+        return model
+
