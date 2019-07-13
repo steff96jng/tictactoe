cce4b3c14e30cde38021cbfae64fadabaae0afb1
mteiting <marcel.teiting@t-online.de>
2019-07-13 21:36:41 +0200

    added png export

--- src/visualize.py
+++ src/visualize.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import jsonpickle
+import os.path
 from math import sqrt
 from stats_proc import EpochData, StatsProcessor
 
-filename = "G:\\stats_current_jhvslxxhwkclmxryjpey.json"
+filename = "e:\\Users\\mteiting\\Downloads\\stats_current_paswtmnvwiuaxmuggcil.json"
+
 
 def read_json_file(path) -> str:
     f = open(path, "r")
@@ -11,14 +13,16 @@ def read_json_file(path) -> str:
     f.close()
     return content
 
+
 def median(list: list) -> float:
     m = 0
 
     for n in list:
         m += n
-    
+
     return m / len(list)
-    
+
+
 def root_mean_square_median(list: list) -> float:
     m = 0
 
@@ -28,7 +32,13 @@ def root_mean_square_median(list: list) -> float:
     return sqrt(m / len(list))
 
 
-def plot_wins(data):
+def get_filename(path):
+    file = os.path.basename(path)
+    file, _ = os.path.splitext(file)
+    return file
+
+
+def plot_wins(data, datafile):
     x_player_win = []
     o_player_win = []
     players_draw = []
@@ -44,24 +54,25 @@ def plot_wins(data):
         rms_x_win.append(root_mean_square_median(x_player_win))
         rms_o_win.append(root_mean_square_median(o_player_win))
         rms_draw.append(root_mean_square_median(players_draw))
-        
 
-    #plt.plot(x_player_win, label='x_player_win')
-    #plt.plot(o_player_win, label='o_player_win')
-    #plt.plot(players_draw, label='players_draw')
-    plt.plot(rms_x_win, label = 'rms_x_win')
-    plt.plot(rms_o_win, label = 'rms_o_win')
-    plt.plot(rms_draw, label = 'rms_draw')
+    # plt.plot(x_player_win, label='x_player_win')
+    # plt.plot(o_player_win, label='o_player_win')
+    # plt.plot(players_draw, label='players_draw')
+    plt.plot(rms_x_win, label="rms_x_win")
+    plt.plot(rms_o_win, label="rms_o_win")
+    plt.plot(rms_draw, label="rms_draw")
 
-    plt.ylabel('Gewinnverteilung')
+    plt.ylabel("Gewinnverteilung")
+    plt.xlabel("Durchläufe")
     plt.legend()
-    plt.show()
+    plt.savefig(get_filename(datafile))
+
+    if hasattr(plt, "show"):
+        plt.show()
+
 
 content = read_json_file(filename)
 data = jsonpickle.decode(content)
 
-plot_wins(data)
-
-
-
+plot_wins(data, filename)
 
